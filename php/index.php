ee85073f4aefce3152a663e20a117edb8ed40038
Oscar David Hernandez Guzman <oscardeveloper0880@gmail.com>
2025-07-19 19:58:47 -0500

    Se realizo el navbar

---
+++ php/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Principal-LAODCOM</title>
+</head>
+<body>
+    <div class="container">
+        <!-- Este es el Sidebar -->
+         <aside class="sidebar">
+            <div class="logo">
+                <h2>LAODCOM</h2>
+                <p>Innovacion Tecnologica a tu alcance</p>
+            </div>
+
+            <nav>
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link active">
+                            <span class="nav-icon">📊</span>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="clientes.php" class="nav-link">
+                            <span class="nav-icon">👥</span>
+                            Clientes
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            <span class="nav-icon">➕</span>
+                            Crear Cliente
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <span class="nav-icon">📈</span>
+                            Reportes
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <span class="nav-icon">⚙️</span>
+                            Configuracion
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            
+         </aside>
+    </div>
+</body>
+</html>
